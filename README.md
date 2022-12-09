2c46950de8893186b36515e12f176f7e25ebab41
hadi <hadi@depository.id>
2022-12-09 22:57:13 +0700

    basic static html

---
+++ README.md
@@ -0,0 +1,7 @@
+## Practicing Golang Html/Template
+
+
+
+### Reference 
+ - [Gin](https://gin-gonic.com/docs/quickstart/)
+ - [Html/Template](https://pkg.go.dev/html/template)
--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import "net/http"
+
+func main() {
+	// http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+	// 	fmt.Fprintf(w, `<h1 style="color:green;">Go world !</h1>`)
+	// 	fmt.Fprintf(w, "<p>Golang is a wonderful languagepppp</p>")
+
+	// })
+	// http.ListenAndServe(":3030", nil)
+	static := http.FileServer(http.Dir("./static"))
+	http.Handle("/", static)
+
+	http.ListenAndServe(":8081", nil)
+}
